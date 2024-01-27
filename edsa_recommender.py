6c968dba21a06ee0a2e21865a2acbec3bc702776
Lucie <nekatilp@gmail.com>
2024-01-27 15:14:45 +0200

    Added images and solution overview

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -27,25 +27,109 @@
 """
 # Streamlit dependencies
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 # Data handling dependencies
 import pandas as pd
 import numpy as np
+import os
+import re
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
 from recommenders.collaborative_based import collab_model
 from recommenders.content_based import content_model
+from PIL import Image
+import base64
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
+
+
+# Background settings
+Logo = Image.open('resources/imgs/logo.png')
+st.set_page_config(page_title='LunaPix', page_icon=Logo)
+
+@st.cache_data
+def get_img_as_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        image_data = image_file.read()
+    return base64.b64encode(image_data).decode()
+
+image = get_img_as_base64('resources/imgs/background.png')
+image1 = get_img_as_base64('resources/imgs/Logo.png')
+
+
+background_image = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{image}");
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+background-attachment: scroll;
+color: black;
+display : flex;
+justify-content: centre;
+align-items: centre;
+}}
+
+[data-testid="stSidebar"] {{
+background-image: url("data:image/png;base64,{image1}");
+background-size: fit;
+background-position: center;
+background-repeat: no-repeat;
+}}
+</style>"""
+st.markdown(background_image, unsafe_allow_html=True)
+
+
+#made with streamlit info
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: show;}
+            footer {visibility: hidden;}
+            
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# -------------------------------------------------------------------
+
+# User account information
+user_email = st.text_input('Enter your email address to create an account:')
+user_password = st.text_input('Enter your password:', type='password', key='password')
+confirm_password = st.text_input('Confirm your password:', type='password', key='confirm_password')
+
+# Validate email format
+email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+if not re.match(email_pattern, user_email):
+    st.warning("Please enter a valid email address.")
+    st.stop()
+
+# Validate password match
+if user_password != confirm_password:
+    st.warning("Passwords do not match. Please confirm your password.")
+    st.stop()
+
+# Age validation
+user_age = st.number_input('Please enter your age', min_value=1, max_value=150, step=1)
+if user_age < 16:
+    st.warning("Sorry, this app is intended for users aged 16 and above. You are not eligible.")
+    st.stop()
+
+# Account creation
+if st.button("Create Account"):
+    st.success(f"Account for {user_email} created successfully!")
+
+# -------------------------------------------------------------------
 
 # App declaration
 def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Solution Overview", "Analysis", "About Us", "Contact Us" ]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -104,6 +188,112 @@ def main():
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
 
+
+    if page_selection == "About Us":
+        st.title("Abous Us")
+
+        selected = option_menu(
+            menu_title = None,
+            options = ["Team", "About the App"],
+            icons = ["person", "app_indicator"], #https://icons.getbootstrap.com/
+            orientation = "horizontal"
+        )
+
+
+        if selected == "Team":
+            st.subheader("Meet Our Team")
+
+
+
+            def display_team_member(image_path, member_name, contact_info, role):
+                st.image(image_path, caption=member_name, width=200)
+                st.write(f"**Name:** {member_name}")
+                st.write(f"**Contact Information:** {contact_info}")
+                st.write(f"**Role:** {role}")
+                st.write("---")
+
+            def about_us():
+                st.title("About Us")
+
+            path_to_images_folder = ("reourses/imgs")
+
+            the_team = [
+                {'image_file': 'Bonani.jpg', 'name': 'Bonani Mkhari', 'contact_info': 'bonanimkhari@gmail.com', 'role': 'Team Lead'},
+                {'image_file': 'Mulalo.jpg', 'name': 'Mulalo Manthanda', 'contact_info': 'manthadamulalo@gmail.com', 'role': 'Project Manager'},
+                {'image_file': 'Lucpah.jpg', 'name': 'Lucpah Nekati', 'contact_info': 'nekatilpi@gmail.com', 'role': 'Software Developer'},
+                {'image_file': 'Lesego.jpg', 'name': 'Lesego Maponyane', 'contact_info': 'lesegomoraladi@gmail.com', 'role': 'Data Scientist'},
+                {'image_file': 'Prince.jpg', 'name': 'Prince Kala', 'contact_info': 'kala33prince@gmail.com', 'role': 'Machine Learning Engineer'},
+                {'image_file': 'Mpho.jpg', 'name': 'Mpho Sesinyi', 'contact_info': 'mphoses@hotmail.com', 'role': 'Data Engineer'},
+            ]
+
+            for member in the_team:
+                image_path = os.path.join(path_to_images_folder, member['image_file'])
+                display_team_member(image_path, member['name'], member['contact_info'], member['role'])
+
+
+        if selected == "About the App":
+            st.subheader("LunaPix")
+            st.markdown(open('resources/pages/About_LunaPix.md').read())
+            st.markdown(open('resources/pages/LunaPix_key_features.md').read())
+            st.markdown(open('resources/pages/Why_LunaPix.md').read())
+            st.markdown(open('resources/pages/LunaPix_vision.md').read())
+
+
+# -------------------------------------------------------------------
+    
+    if page_selection == "Analysis":
+        st.title("Exploratory Data Analysis")
+        st.write("Behind the Scenes: Understanding Movie Preferences through EDA")
+
+        selected =  option_menu(
+            menu_tile =  None,
+            options = ["Insights", "Movie Titles Overview"],
+            icons = ["database-fill-check", "film"], #https://icons.getbootstrap.com/
+            orientation = "horizontal"
+        )
+
+        if selected ==  "Insights":
+            st.title("Behind The Scenes")       
+
+            if st.checkbox("Top 10 users by number of ratings"):
+                st.title("The Top 10 Users by Number of Ratings")
+                st.image("resources/visuals/topuser.png", use_column_width=True)
+
+
+            if st.checkbox("Top 20 movies viewed by users"):
+                st.title("Top 20 movies viewed by users")
+                st.image("resources/visuals/topviewed.png", use_column_width=True)
+
+
+            if st.checkbox("Number of movies per director"):
+                st.title("Top 10 directors and their movie counts")
+                st.image("resources/visuals/directormovies.png", use_column_width=True)
+
+
+            if st.checkbox("Top 10 Directors with Highest Rated Movies"):
+                st.title("Top 10 Directors with Highest Rated Movies")
+                st.image("resources/visuals/topdirectors.png", use_column_width=True)
+
+            if st.checkbox("Top 10 Directors with Worst Rated Movies"):
+                st.title("Top 10 Directors with Worst Rated Movies")
+                st.image("resources/visuals/worstdirectors.png", use_column_width=True)
+
+
+            if st.checkbox("Top 10 Highest Rated Movies"):
+                st.title("The Top 10 Highest Rated Movies")
+                st.image("resources/visuals/topmovies.png", use_column_width=True)
+
+
+            if st.checkbox("Top 10 Worst Rated Movies"):
+                st.title("The Top 10 Worst Rated Movies")
+                st.image("resources/visuals/worstmovies.png", use_column_width=True)
+
+
+            if st.checkbox("Most common genres"):
+                st.title("Most common genres")
+                st.image("resources/visuals/genres.png", use_column_width=True)
+
+            
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
 
---
+++ resources/pages/LunaPix_vision.md
@@ -0,0 +1,9 @@
+**The Future: Emotional Intelligence Integration**
+
+LunaPix will take a revolutionary step by incorporating emotional intelligence into the recommendation process. While most systems focus on past choices, LunaPix will delve into users' current emotional states. By simulating moods and desires through predefined mood selections, LunaPix will craft a unique emotional map, guiding users to movies that resonate with their feelings at the moment.
+
+With users emotions at the centre of LunaPix's to emotional intelligence, it will incorporate Brain-Computer Interface (BCI) technology to provide a movie recommender system that understands users' emotions, anxieties, and unspoken longings. Leveraging BCI, LunaPix will create a captivating mood board that serves as a visual representation of users' emotional states. This will ensure less reliance on past viewing choices. It also aims to incorporate a hybrid recommender system, which together with the BCI will work to overcome the cold start approach as there will be less reliance of what the user has watched before. So, even if you are new to LunaPix, you will feel familiar as part of its concern is on your present emotional state. 
+
+LunaPix, with its emotional approach to movie recommendations, has the potential to spark a viral sensation and position Star-Arc Labz as a pioneer in human-centric tech. The app will go beyond traditional recommender systems, fostering a deep connection between users and the cinematic orchestra of their emotions.
+
+In essence, LunaPix is not just an app; it's a transformative experience that redefines how individuals interact with and discover cinematic content, opening doors to a world of unexpected beauty and joy.
